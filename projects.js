fbbf76ce3aa02af1dcee18da5f9f11b2df462c8b
August Wanner <32688424+wannere@users.noreply.github.com>
2023-01-04 15:12:47 -0800

    2023 update

--- projects.js
+++ projects.js
@@ -1,26 +1,9 @@
 $(document).ready(function(){
-  function adjustWidth() {
-     var parentwidth = $(".container").width();
-     $(".second-nav").width(parentwidth);
-  };
   $(".menu").click(function(){
     $(".menu").toggleClass("is-active");
-    $(".item-wrapper").slideToggle('fast').css("display", "flex");
-    $(".overlay").fadeToggle('fast');
-    $(".header_line").fadeToggle('fast').css("display", "block");
+    $(".dropdown_container").slideToggle('fast').css("display", "flex");
   });
-  $(".nav-item").click(function(){
-    $(".menu").toggleClass("is-active");
-    $(".item-wrapper").slideToggle('fast').css("display", "flex");
-    if($(window).width() < 550 ) {
-      $(".overlay").fadeToggle('fast');
-      $(".overlay").css("display", "none");
-    };
-  });
-  $(window).resize(function(){
-    adjustWidth();
-    if($(window).width() > 550) {
-      $(".item-wrapper").css("display", "none");
-    };
+  $("#top").click(function(){
+    $("html, body").animate({scrollTop: 0}, 1000);
   });
 });
