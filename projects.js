8acecc810f37919f0527068f425029ffac2140e6
August Wanner <32688424+wannere@users.noreply.github.com>
2023-09-12 14:00:50 -0700

    button edits
    adjusted arrow hover for next/prev buttons. moved back to top button into footer.

--- projects.js
+++ projects.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
     $(".dropdown_container").slideToggle('fast').css("display", "flex");
   });
   $("#top").click(function(){
-    $("html, body").animate({scrollTop: 0}, 1000);
+    $("html, body").animate({scrollTop: 0}, 100);
   });
 });
